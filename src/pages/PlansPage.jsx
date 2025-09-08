9c35e680cfe02df7b1878051db11ad84b37d73c7
Paulo Duarte <parcerias.pduarte@gmail.com>
2025-09-07 21:49:13 -0300

    feat: icons plans

--- src/pages/PlansPage.jsx
+++ src/pages/PlansPage.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
 import { useAuth } from '../context/AuthContext';
+import {SignalLow, SignalMedium, SignalHigh} from 'lucide-react';
 
 const apiUrl = import.meta.env.VITE_API_URL;
 
@@ -57,7 +58,7 @@ export const PlansPage = () => {
             <h1>Nossos Planos</h1>
             <PlanCard>
                 <h2>Plano Power</h2>
-                <p>Ideal para começar.</p>
+                <p><SignalLow />Ideal para começar.</p>
                 {/* Substitua pelo ID do Preço que você copiou do Stripe */}
                 <PlanButton onClick={() => handleCheckout('price_1S4otg38EcxtIJ87v7Q5iwyP')}>
                     Assinar Agora
@@ -65,7 +66,7 @@ export const PlansPage = () => {
             </PlanCard>
             <PlanCard>
                 <h2>Plano Turbo</h2>
-                <p>Ideal para Campanhas.</p>
+                <p><SignalMedium />Ideal para Campanhas.</p>
                 {/* Substitua pelo ID do Preço que você copiou do Stripe */}
                 <PlanButton onClick={() => handleCheckout('price_1S4ouD38EcxtIJ87eaRNGMOW')}>
                     Assinar Agora
@@ -73,21 +74,12 @@ export const PlansPage = () => {
             </PlanCard>
             <PlanCard>
                 <h2>Plano Ultra</h2>
-                <p>Ideal para Agencias que Gerenciam Varios Clientes</p>
+                <p><SignalHigh />Ideal para Agencias</p>
                 {/* Substitua pelo ID do Preço que você copiou do Stripe */}
                 <PlanButton onClick={() => handleCheckout('price_1S4out38EcxtIJ87KG0DUNcf')}>
                     Assinar Agora
                 </PlanButton>
             </PlanCard>
-            <PlanCard>
-                <h2>Plano Free</h2>
-                <p>Ideal para conhecer.</p>
-                {/* Substitua pelo ID do Preço que você copiou do Stripe */}
-                <PlanButton onClick={() => handleCheckout('price_1S4ovp38EcxtIJ8776wx3dum')}>
-                    Assinar Agora
-                </PlanButton>
-            </PlanCard>
-            {/* Adicione outros PlanCards para os planos Turbo e Ultra */}
         </PlansWrapper>
     );
 };
